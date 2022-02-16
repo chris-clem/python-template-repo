8b9c9508d12c3f73853c772f1a3c47e76516b74a
Christoph Clement <christoph.clement@students.unibe.ch>
2022-02-16 18:53:25 +0100

    Remove click and add fire

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.script_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.script_name}}.py
@@ -1,9 +1,5 @@
-from click.testing import CliRunner
-
 from {{cookiecutter.package_name}}.{{cookiecutter.script_name}} import main
 
 
 def test_main():
-    runner = CliRunner()
-    result = runner.invoke(main)
-    assert result.exit_code == 0
+    main()
--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.script_name}}.py
@@ -1,33 +1,14 @@
-from pprint import pformat
 from time import sleep
 
-import click
 from loguru import logger
 from tqdm import tqdm
 
 
-@click.command()
-@click.option(
-    "-l",
-    "--len_loop",
-    default=100,
-    type=click.INT,
-    show_default=True,
-    help="Length of the tqdm loop",
-)
-@click.option(
-    "-s",
-    "--sleep_seconds",
-    default=0.01,
-    type=click.FLOAT,
-    show_default=True,
-    help="Amount of seconds to sleep",
-)
-def main(**kwargs):
-    logger.info(f"kwargs:\n{pformat(kwargs)}")
+def main(len_loop: int = 10, sleep_secs: int = 1):
+    logger.info(f"{len_loop=}, {sleep_secs=}")
 
-    for _ in tqdm(range(kwargs["len_loop"])):
-        sleep(kwargs["sleep_seconds"])
+    for _ in tqdm(range(len_loop)):
+        sleep(sleep_secs)
 
 
 if __name__ == "__main__":
