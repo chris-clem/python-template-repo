189e6786733ad040cc46c8dac0ca8bc4631e767b
Christoph Clement <christoph.clement@students.unibe.ch>
2022-03-31 12:22:48 +0200

    Use fire to start script

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.script_name}}.py
@@ -1,10 +1,15 @@
 from time import sleep
 
+import fire
 from loguru import logger
 from tqdm import tqdm
 
 
-def main(len_loop: int = 10, sleep_secs: int = 1):
+def main():
+    fire.Fire({{cookiecutter.script_name}})
+
+
+def {{cookiecutter.script_name}}(len_loop: int = 10, sleep_secs: int = 1):
     logger.info(f"{len_loop=}, {sleep_secs=}")
 
     for _ in tqdm(range(len_loop)):
